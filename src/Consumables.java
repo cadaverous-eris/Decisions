04f3d8ad99d817710393b2aa7a0b55a56c43378d
clydebaxterII <stuartlindstrom@gmail.com>
2015-05-20 19:33:29 -0400

    singularized plural classes

--- src/Consumables.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by BLUE on 20.05.15.
- */
-public class Consumables {
-	public Consumables() {
-		
-	}
-}
--- src/Equipable.java
+++ src/Equipable.java
@@ -1,5 +1,5 @@
 /**
  * Created by BLUE on 20.05.15.
  */
-public class Equipables {
+public class Equipable {
 }
