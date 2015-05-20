26e1f210efe028fed4b809532150694790ea3de6
nfishman <njwfish@gmail.com>
2015-05-20 06:24:24 -0400

    Added content
    Added start of childhood

--- src/Life.java
+++ src/Life.java
@@ -11,27 +11,43 @@ public class Life{
     }
     private int decide(String[] choices){
         int choice;
-        do{
-            out("Your options are:");
             for (int i = 0; i < choices.length; i++)
-                out(choices[i]);
-            choice = (new Scanner(System.in)).nextInt();
+                out("[" + (i + 1) + "]" + choices[i]);
+        do{
+            choice = (new Scanner(System.in)).nextInt() - 1;
         }while (choice == -1);
         return choice;
     }
 
+    public void init(){
+        birth();
+    }
+
     private void birth(){
         characters.add(new Character());
-        if (characters.get(0).getLuck() > 0) out("\nYou are born, it is a rather unremarkable affair, and you, are a rather unremarkable baby.\n");
+        if (characters.get(0).getLuck() > 0){
+            out("\nYou are born, it is a rather unremarkable affair, and you, are a rather unremarkable baby.\n");
+            childhood();
+        }
         out("\nYou are still born");
         death();
     }
+    private void childhood(){
+        out("As you grow up, your life remains relatively dull.\n\nIn middle school you wake up every morning and eat:\n\n");
+        switch(decide(("Cereal\n,Eggs and Bacon\n,Oatmeal\n").split(", ?"))){
+            case 0:
+                characters.get(0).setWeight(characters.get(0).getWeight() + 20);
+                break;
+            case 1:
+                characters.get(0).setWeight(characters.get(0).getWeight() + 5);
+                characters.get(0).setEndurance(characters.get(0).getEndurance() + 1);
+                break;
+            default:
+                break;
+        }
+    }
     private void death(){
         out("You die sad and alone.");
-    }
-    private void childhood(){
-
-
     }
 
 }
